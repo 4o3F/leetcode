0a5238710cd42accf79297f9db5441de83544fe5
4o3F <4o3f@proton.me>
2024-10-09 21:07:30 +0800

    feat: problem 921 & 1963

---
+++ leetcode/src/problems/problem_921.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn min_add_to_make_valid(s: String) -> i32 {
+        let (mut balance, mut count) = (0, 0);
+        for c in s.chars() {
+            if c == '(' {
+                balance += 1;
+            } else if balance > 0 {
+                balance -= 1;
+            } else {
+                count += 1;
+            }
+        }
+        count + balance
+    }
+}
+
+struct Solution {}
+pub fn run() {
+    tracing::info!("{}", Solution::min_add_to_make_valid("())".to_string()));
+}
