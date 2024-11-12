4f95a60d7e6646dc76d6f3f3e92d77430e3b8e7e
4o3F <4o3f@proton.me>
2024-11-12 19:23:39 +0800

    feat: problem 2070

---
+++ leetcode/src/problems/problem_2070.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    pub fn maximum_beauty(mut items: Vec<Vec<i32>>, queries: Vec<i32>) -> Vec<i32> {
+        items.sort_unstable();
+        items.dedup_by(|right, left| right[1] <= left[1]);
+        queries
+            .into_iter()
+            .map(|q| match items.partition_point(|it| it[0] <= q) {
+                0 => 0,
+                i => items[i - 1][1],
+            })
+            .collect()
+    }
+}
+
+struct Solution {}
+pub fn run() {
+    let items = vec![[1, 2], [3, 2], [2, 4], [5, 6], [3, 5]]
+        .into_iter()
+        .map(|slice| slice.to_vec())
+        .collect();
+    let queries = vec![1, 2, 3, 4, 5, 6];
+    tracing::info!("{:?}", Solution::maximum_beauty(items, queries))
+}
