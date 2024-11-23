87223380afa40bde9e8459d88e64e20cad6ef577
4o3F <4o3f@proton.me>
2024-11-23 15:47:05 +0800

    feat: problem 1072 & 1861 & 2257

---
+++ leetcode/src/problems/problem_1072.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn max_equal_rows_after_flips(matrix: Vec<Vec<i32>>) -> i32 {
+        let row = matrix.len();
+        let col = matrix[0].len();
+        let mut row_chunks = Vec::<Vec<i32>>::new();
+        for row_idx in 0..row {
+            let row = &matrix[row_idx];
+            let mut last_occur: Option<i32> = None;
+            let mut count = 0;
+            let mut chunk = Vec::<i32>::new();
+            for col_idx in 0..col {
+                match row[col_idx] {
+                    0 => {
+                        if let Some(current_last_occur) = last_occur {
+                            if current_last_occur == 0 {
+                                count += 1;
+                            } else {
+                                chunk.push(count);
+                                last_occur = Some(0);
+                                count = 1;
+                            }
+                        } else {
+                            last_occur = Some(0);
+                            count = 1;
+                        }
+                    }
+                    1 => {
+                        if let Some(current_last_occur) = last_occur {
+                            if current_last_occur == 1 {
+                                count += 1;
+                            } else {
+                                chunk.push(count);
+                                last_occur = Some(1);
+                                count = 1;
+                            }
+                        } else {
+                            last_occur = Some(1);
+                            count = 1;
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            chunk.push(count);
+            row_chunks.push(chunk);
+        }
+        let mut frequency = HashMap::<Vec<i32>, i32>::new();
+        // tracing::debug!("{:?}", row_chunks);
+        row_chunks.into_iter().for_each(|chunk| {
+            *frequency.entry(chunk).or_insert(0) += 1;
+        });
+        // tracing::debug!("{:?}", frequency);
+        frequency
+            .into_iter()
+            .max_by_key(|(_, count)| *count)
+            .unwrap()
+            .1
+    }
+}
+
+struct Solution {}
+
+pub fn run() {
+    let arr = vec![
+        vec![1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        vec![1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        vec![1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+        vec![1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        vec![1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+    ];
+    tracing::info!("{}", Solution::max_equal_rows_after_flips(arr));
+    let arr = vec![vec![0, 0, 0], vec![0, 0, 1], vec![1, 1, 0]];
+    tracing::info!("{}", Solution::max_equal_rows_after_flips(arr));
+}
---
+++ leetcode/src/problems/problem_2257.rs
@@ -0,0 +1,117 @@
+#[derive(Debug)]
+enum State {
+    Wall,
+    Guard,
+    Occupied,
+    Unoccupied,
+}
+
+impl Solution {
+    pub fn count_unguarded(m: i32, n: i32, guards: Vec<Vec<i32>>, walls: Vec<Vec<i32>>) -> i32 {
+        let mut field = Vec::<Vec<State>>::new();
+        for _ in 0..m {
+            let mut row = Vec::<State>::new();
+            for _ in 0..n {
+                row.push(State::Unoccupied);
+            }
+            field.push(row);
+        }
+        for wall in walls {
+            let x = wall[0] as usize;
+            let y = wall[1] as usize;
+            field[x][y] = State::Wall;
+        }
+
+        for guard in &guards {
+            let x = guard[0] as usize;
+            let y = guard[1] as usize;
+            field[x][y] = State::Guard;
+        }
+
+        // tracing::debug!("{:?}", field);
+        for guard in guards {
+            let row = guard[0] as usize;
+            let col = guard[1] as usize;
+            // tracing::debug!("guard {} {}", row, col);
+            // left
+            for col_idx in (0..col).rev() {
+                match field[row][col_idx] {
+                    State::Guard => break,
+                    State::Wall => break,
+                    _ => field[row][col_idx] = State::Occupied,
+                }
+            }
+
+            // tracing::debug!("left {:?}", field);
+            // right
+            for col_idx in col + 1..(n as usize) {
+                match field[row][col_idx] {
+                    State::Guard => break,
+                    State::Wall => break,
+                    _ => field[row][col_idx] = State::Occupied,
+                }
+            }
+            // tracing::debug!("right {:?}", field);
+            // up
+            for row_idx in (0..row).rev() {
+                // tracing::debug!("y_idx up {}", row_idx);
+                match field[row_idx][col] {
+                    State::Guard => break,
+                    State::Wall => break,
+                    _ => field[row_idx][col] = State::Occupied,
+                }
+            }
+
+            // tracing::debug!("up {:?}", field);
+            // down
+            for row_idx in row + 1..(m as usize) {
+                // tracing::debug!("row_idx down {}", row_idx);
+                match field[row_idx][col] {
+                    State::Guard => break,
+                    State::Wall => break,
+                    _ => field[row_idx][col] = State::Occupied,
+                }
+            }
+            // tracing::debug!("down {:?}", field);
+        }
+        // tracing::debug!("{:?}", field);
+
+        field
+            .iter()
+            .flat_map(|row| row.iter())
+            .filter(|&state| matches!(state, State::Unoccupied))
+            .count() as i32
+    }
+}
+
+struct Solution {}
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::count_unguarded(
+            5,
+            5,
+            vec![vec![1, 4], vec![4, 1], vec![0, 3]],
+            vec![vec![3, 2]]
+        )
+    );
+    // return;
+    tracing::info!(
+        "{}",
+        Solution::count_unguarded(
+            2,
+            7,
+            vec![vec![1, 5], vec![1, 1], vec![1, 6], vec![0, 2]],
+            vec![vec![0, 6], vec![0, 3], vec![0, 5]]
+        )
+    );
+    tracing::info!(
+        "{}",
+        Solution::count_unguarded(
+            4,
+            6,
+            vec![vec![0, 0], vec![1, 1], vec![2, 3]],
+            vec![vec![0, 1], vec![2, 2], vec![1, 4]]
+        )
+    );
+}
