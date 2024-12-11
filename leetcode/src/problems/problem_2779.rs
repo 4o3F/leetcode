c10af50e3f3c284a42505be335a60373617fdc6c
4o3F <4o3f@proton.me>
2024-12-11 15:15:58 +0800

    feat: problem 2779

---
+++ leetcode/src/problems/problem_2779.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn maximum_beauty(mut nums: Vec<i32>, k: i32) -> i32 {
+        nums.sort_unstable();
+        let mut result = 0;
+        for (index, num) in nums.iter().enumerate() {
+            let point = nums.partition_point(|n| n - num <= 2 * k);
+            result = result.max(point - index);
+        }
+        result as i32
+    }
+}
+struct Solution {}
+pub fn run() {
+    tracing::info!("{}", Solution::maximum_beauty(vec![4, 6, 1, 2], 2));
+    tracing::info!("{}", Solution::maximum_beauty(vec![1, 1, 1, 1], 10));
+    tracing::info!("{}", Solution::maximum_beauty(vec![49, 26], 12));
+}
