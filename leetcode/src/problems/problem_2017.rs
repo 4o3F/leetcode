f4c9a7d4a6ed4c82669b428f8d17466ee92a12c2
4o3F <4o3f@proton.me>
2025-01-23 22:11:10 +0800

    feat: problem 1267 & 1765 & 2017 & 2661

---
+++ leetcode/src/problems/problem_2017.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
+        let (mut a, mut b) = (0, 0);
+        for &v in grid[0].iter() {
+            a += v as i64
+        }
+        (0..grid[0].len())
+            .map(|x| {
+                a -= grid[0][x] as i64;
+                let m = a.max(b);
+                b += grid[1][x] as i64;
+                m
+            })
+            .min()
+            .unwrap()
+    }
+}
+
+struct Solution;
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::grid_game(vec![vec![2, 5, 4], vec![1, 5, 1]])
+    );
+}
