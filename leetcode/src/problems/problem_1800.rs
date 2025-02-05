e4cef3d7234734b3ab59d9bffad95b4690eab31e
4o3F <4o3f@proton.me>
2025-02-05 11:53:23 +0800

    feat: problem 1752 & 3105 & 3151& 1790

---
+++ leetcode/src/problems/problem_1800.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn max_ascending_sum(nums: Vec<i32>) -> i32 {
+        nums.chunk_by(|a, b| a < b)
+            .map(|v| v.iter().sum())
+            .max()
+            .unwrap_or(0)
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::max_ascending_sum(vec![10, 20, 30, 5, 10, 50])
+    );
+}
