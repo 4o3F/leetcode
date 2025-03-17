10521c49b89fe707f35679de663ff81f743d8b47
4o3F <4o3f@proton.me>
2025-03-17 21:42:23 +0800

    feat: problem 2206 & 2594

---
+++ leetcode/src/problems/problem_2206.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn divide_array(nums: Vec<i32>) -> bool {
+        nums.iter()
+            .fold(HashMap::<i32, i32>::new(), |mut f, n| {
+                *f.entry(*n).or_default() += 1;
+                f
+            })
+            .into_values()
+            .all(|x| x % 2 == 0)
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{:?}", Solution::divide_array(vec![3,2,3,2,2,2]));
+}
