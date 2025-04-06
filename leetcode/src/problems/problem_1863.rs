e7123ae83f20638c9e2a6c4f1a39a3569eccd525
403F <4o3f@proton.me>
2025-04-06 21:27:34 +0800

    feat: problem 368 & 1863

---
+++ leetcode/src/problems/problem_1863.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        nums.iter().fold(0, |x, &y| x | y) * (1 << (nums.len() - 1))
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::subset_xor_sum(vec![3, 4, 5, 6, 7, 8]))
+}
