d52510bc2167f5ced12bc7d02c6e9239b34e529c
403F <4o3f@proton.me>
2025-04-18 10:33:21 +0800

    feat: problem 38 & 2176

---
+++ leetcode/src/problems/problem_2176.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let length = nums.len();
+
+        let mut count = 0;
+        for i in 0..length {
+            for j in i + 1..length {
+                if nums[i] == nums[j] && (i * j) as i32 % k == 0 {
+                    count += 1;
+                    // tracing::info!("{} {}", i, j);
+                }
+            }
+        }
+        count
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::count_pairs(vec![3, 1, 2, 2, 2, 1, 3], 2));
+}
