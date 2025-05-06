152b0ce88c74cafac47eb4942e9e08ebbef6c8db
403F <4o3f@proton.me>
2025-05-06 16:35:23 +0800

    feat: problem 790 & 1007 & 1128 & 1920

---
+++ leetcode/src/problems/problem_1920.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut result = Vec::new();
+
+        for &idx in nums.iter() {
+            result.push(nums[idx as usize]);
+        }
+        result
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{:?}", Solution::build_array(vec![0, 2, 1, 5, 3, 4]));
+}
