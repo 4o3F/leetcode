3860d45458dc18714ef22274f680b502ff1de578
403F <4o3f@proton.me>
2025-05-12 09:52:54 +0800

    feat: problem 1550 & 2094 & 2918 & 3343

---
+++ leetcode/src/problems/problem_2094.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn find_even_numbers(digits: Vec<i32>) -> Vec<i32> {
+        let mut digits_map = HashMap::<i32, i32>::new();
+        for digit in digits {
+            let entry = digits_map.entry(digit).or_insert(0);
+            *entry += 1;
+        }
+
+        let mut results = Vec::<i32>::new();
+        'outer: for result in (100..=999).step_by(2) {
+            let mut num = result;
+            let mut count = vec![0; 10];
+            while num != 0 {
+                let last_digit = num % 10;
+                count[last_digit as usize] += 1;
+                if !digits_map.contains_key(&last_digit)
+                    || digits_map.get(&last_digit).unwrap() < &count[last_digit as usize]
+                {
+                    continue 'outer;
+                }
+                num /= 10;
+            }
+            results.push(result);
+        }
+        results
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{:?}", Solution::find_even_numbers(vec![2, 2, 8, 8, 2]))
+}
