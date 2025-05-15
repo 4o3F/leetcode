e951f50e6c8ee00b9b44df24af009363b702fd88
403F <4o3f@proton.me>
2025-05-15 09:44:37 +0800

    feat: problem 2900 & 3335 & 3337

---
+++ leetcode/src/problems/problem_3335.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn length_after_transformations(s: String, t: i32) -> i32 {
+        const MODULO: i32 = 1_000_000_007;
+        let mut freq = vec![0i32; 26];
+        let s = s.into_bytes();
+        for c in s {
+            freq[(c - b'a') as usize] += 1;
+        }
+        for _ in 0..t {
+            let added_ab = freq[25];
+            for i in (1..26).rev() {
+                freq[i] = freq[i - 1] % MODULO;
+            }
+            freq[0] = added_ab % MODULO;
+            freq[1] += added_ab % MODULO;
+            // tracing::info!("{:?}", freq);
+        }
+        freq.iter()
+            .fold(0i64, |sum, num| (sum + i64::from(*num)) % i64::from(MODULO)) as i32
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::length_after_transformations("iatckelagcytuuxbgiajuoni".to_string(), 6626)
+    );
+}
