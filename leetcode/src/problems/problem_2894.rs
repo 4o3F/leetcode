a19d8d59c8f5eea94bab48c7cbff783183dc1a96
403F <4o3f@proton.me>
2025-05-27 09:01:43 +0800

    feat: problem 1857 & 2131 & 2894

---
+++ leetcode/src/problems/problem_2894.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let (a, b) = (1..=n).fold((0, 0), |(not_divisible, divisible), num| {
+            if num % m == 0 {
+                (not_divisible, divisible + num)
+            } else {
+                (not_divisible + num, divisible)
+            }
+        });
+        a - b
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::difference_of_sums(10, 3));
+}
