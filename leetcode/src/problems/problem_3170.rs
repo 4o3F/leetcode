39b2754b08ee6c0caf0fcefec0fd4672ff337d04
403F <4o3f@proton.me>
2025-06-08 00:50:02 +0800

    feat: problem 3170

---
+++ leetcode/src/problems/problem_3170.rs
@@ -0,0 +1,36 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn clear_stars(s: String) -> String {
+        let mut h: BinaryHeap<(Reverse<char>, usize)> = BinaryHeap::new();
+        for (i, c) in s.chars().enumerate() {
+            if c == '*' {
+                h.pop();
+            } else {
+                h.push((Reverse(c), i));
+            }
+        }
+
+        if h.is_empty() {
+            return String::from("");
+        }
+
+        let max = h.iter().map(|(_, i)| *i).max().unwrap_or(0);
+
+        let mut x = vec!['\0'; max + 1];
+
+        while let Some((Reverse(c), i)) = h.pop() {
+            x[i] = c;
+        }
+
+        let result: String = x.iter().filter(|&&c| c != '\0').collect();
+        result
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::clear_stars("aaba*".to_string()))
+}
