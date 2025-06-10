cc4a5287bf090eb125fc0aaeab52b43f8967d83f
403F <4o3f@proton.me>
2025-06-10 09:04:04 +0800

    feat: problem 3442

---
+++ leetcode/src/problems/problem_3442.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn max_difference(s: String) -> i32 {
+        let freq = s.chars().fold(HashMap::<char, i32>::new(), |mut freq, c| {
+            let entry = freq.entry(c).or_insert(0);
+            *entry += 1;
+            freq
+        });
+        let (mut max_odd, mut min_even) = (0, i32::MAX);
+        for freq in freq.values() {
+            if freq % 2 == 0 {
+                min_even = min_even.min(*freq);
+            } else {
+                max_odd = max_odd.max(*freq);
+            }
+        }
+        max_odd - min_even
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::max_difference("aaaaabbc".to_string()));
+}
