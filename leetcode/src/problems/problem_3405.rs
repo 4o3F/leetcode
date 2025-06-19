033c0d5a813ca5100a2dd0d3a916738c9bc7eb89
403F <4o3f@proton.me>
2025-06-19 21:01:46 +0800

    feat: problem 2294 & 2966 & 3405

---
+++ leetcode/src/problems/problem_3405.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn count_good_arrays(n: i32, m: i32, k: i32) -> i32 {
+        let (m_const, n, m, k, mut ncr, mut den) =
+            (1_000_000_007, n as i64, m as i64, k as i64, 1, 1);
+        fn pow(a: i64, b: i64, m_const: i64) -> i64 {
+            if b == 0 {
+                1
+            } else {
+                (pow((a * a) % m_const, b / 2, m_const) * if b % 2 > 0 { a } else { 1 }) % m_const
+            }
+        }
+        for i in 1..=k {
+            ncr = (ncr * (n - i)) % m_const;
+            den = (den * i) % m_const
+        }
+        ncr = (ncr * pow(den, m_const - 2, m_const)) % m_const;
+        ((((m * ncr) % m_const) * pow(m - 1, n - k - 1, m_const)) % m_const) as i32
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::count_good_arrays(3, 2, 1))
+}
