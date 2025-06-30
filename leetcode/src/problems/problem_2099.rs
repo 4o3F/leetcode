0389c9f765f6bb1481f904444cbfdead25cc93f9
403F <4o3f@proton.me>
2025-06-30 20:41:01 +0800

    feat: problem 594 & 1498 & 2099

---
+++ leetcode/src/problems/problem_2099.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn max_subsequence(mut n: Vec<i32>, k: i32) -> Vec<i32> {
+        for i in 0..n.len() {
+            n[i] = (n[i] << 11) | i as i32
+        }
+        n.sort_unstable();
+        let l = n.len() - k as usize;
+        (&mut n[l..]).sort_unstable_by_key(|x| x & ((1 << 11) - 1));
+        for x in &mut n {
+            *x >>= 11
+        }
+        n[l..].to_vec()
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{:?}", Solution::max_subsequence(vec![2, 1, 3, 3], 2))
+}
