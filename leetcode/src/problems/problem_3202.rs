2b5e28d4845099a6e3437be63fadc32d5c4642bf
403F <4o3f@proton.me>
2025-07-20 13:19:02 +0800

    feat: problem 1948 & 2163 & 3201 & 3202

---
+++ leetcode/src/problems/problem_3202.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn maximum_length(n: Vec<i32>, k: i32) -> i32 {
+        (0..k as usize)
+            .map(|v| {
+                let k = k as usize;
+                let mut len = vec![0; k];
+                n.iter()
+                    .map(|&x| {
+                        let x = x as usize;
+                        len[x % k] = len[x % k].max(1 + len[(k + v - x % k) % k]);
+                        len[x % k]
+                    })
+                    .max()
+                    .unwrap()
+            })
+            .max()
+            .unwrap() as _
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{}", Solution::maximum_length(vec![1, 2, 3, 4, 5], 2))
+}
