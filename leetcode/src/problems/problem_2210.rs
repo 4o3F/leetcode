0f4bdf2b38f2bbda180bae1b20ca1240d326fc03
403F <4o3f@proton.me>
2025-07-29 22:41:13 +0800

    feat: problem 2210 & 2322 & 2411 & 3480 & 3487

---
+++ leetcode/src/problems/problem_2210.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+
+impl Solution {
+    pub fn count_hill_valley(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .dedup()
+            .tuple_windows::<(_, _, _)>()
+            .filter(|(a, b, c)| (a > b) == (b < c))
+            .count() as _
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::count_hill_valley(vec![1, 1, 2, 2, 2, 2, 2, 1])
+    )
+}
---
+++ leetcode/src/problems/problem_2411.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn smallest_subarrays(n: Vec<i32>) -> Vec<i32> {
+        let (mut j, mut r) = ([0; 30], vec![0; n.len()]);
+        for i in (0..n.len()).rev() {
+            for b in 0..30 {
+                if n[i] >> b & 1 > 0 {
+                    j[b] = i
+                }
+            }
+            r[i] = 1.max(1 + *j.iter().max().unwrap() as i32 - i as i32)
+        }
+        r
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!("{:?}", Solution::smallest_subarrays(vec![1, 0, 2, 1, 3]))
+}
