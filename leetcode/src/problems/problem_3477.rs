43dcbbca17a82c061b9990b314b6dd649d066370
403F <4o3f@proton.me>
2025-08-06 23:43:10 +0800

    feat: problem 3477 & 3479

---
+++ leetcode/src/problems/problem_3477.rs
@@ -0,0 +1,38 @@
+impl Solution {
+    pub fn num_of_unplaced_fruits(fruits: Vec<i32>, baskets: Vec<i32>) -> i32 {
+        let mut taken = vec![false; baskets.len()];
+
+        let mut result = 0;
+        let mut placed = 0;
+        for fruit in fruits {
+            if placed == baskets.len() {
+                result += 1;
+                continue;
+            }
+            let mut flag = false;
+            for idx in 0..baskets.len() {
+                if taken[idx] {
+                    continue;
+                } else if baskets[idx] >= fruit {
+                    taken[idx] = true;
+                    placed += 1;
+                    flag = true;
+                    break;
+                }
+            }
+            if !flag {
+                result += 1;
+            }
+        }
+        result
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::num_of_unplaced_fruits(vec![4, 2, 5], vec![3, 5, 4])
+    )
+}
