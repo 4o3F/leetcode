830aeac1721be2ced9e75cf16717eaca7d7d4e75
403F <4o3f@proton.me>
2025-09-29 15:56:46 +0800

    feat: problem 812 & 976 & 1039

---
+++ leetcode/src/problems/problem_1039.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn min_score_triangulation(values: Vec<i32>) -> i32 {
+        let n = values.len();
+        let mut dp = vec![vec![0; n]; n];
+        for j in 2..n {
+            for i in (0..j - 1).rev() {
+                dp[i][j] = i32::MAX;
+                for k in i + 1..j {
+                    dp[i][j] =
+                        dp[i][j].min(dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
+                }
+            }
+        }
+        dp[0][n - 1]
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::min_score_triangulation(vec![1, 3, 1, 4, 1, 5])
+    )
+}
