a3889acdc28dc08c5cc1a0347a298fca386f7354
403F <4o3f@proton.me>
2025-10-15 21:01:51 +0800

    feat: problem 2273 & 3147 & 3186 & 3349 & 3350 & 3494 & 3539

---
+++ leetcode/src/problems/problem_3350.rs
@@ -0,0 +1,22 @@
+use itertools::Itertools;
+use std::iter::once;
+
+impl Solution {
+    pub fn max_increasing_subarrays(nums: Vec<i32>) -> i32 {
+        once(0)
+            .chain(nums.chunk_by(|a, b| a < b).map(|c| c.len()))
+            .tuple_windows()
+            .map(|(ck1, ck2)| ck1.min(ck2).max(ck2 / 2))
+            .max()
+            .unwrap() as i32
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::max_increasing_subarrays(vec![2, 5, 7, 8, 9, 2, 3, 4, 3, 1])
+    )
+}
