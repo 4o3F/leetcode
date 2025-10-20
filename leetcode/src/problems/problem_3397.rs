f3e0fb44e5fef826a211d8c6f82665906c7ba560
403F <4o3f@proton.me>
2025-10-21 01:02:47 +0800

    feat: problem 1625 & 2011 & 3003 & 3397

---
+++ leetcode/src/problems/problem_3397.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn max_distinct_elements(mut nums: Vec<i32>, k: i32) -> i32 {
+        nums.sort();
+        let mut prev_assigned: i64 = -9_000_000_000_000_000;
+        let mut distinct_count: i32 = 0;
+        for &v in nums.iter() {
+            let num = v as i64;
+            let assigned = std::cmp::max(num - k as i64, prev_assigned + 1);
+            if assigned <= num + k as i64 {
+                distinct_count += 1;
+                prev_assigned = assigned;
+            }
+        }
+        distinct_count
+    }
+}
+
+struct Solution;
+
+pub fn run() {
+    tracing::info!(
+        "{}",
+        Solution::max_distinct_elements(vec![1, 2, 2, 3, 3, 4], 2)
+    )
+}
